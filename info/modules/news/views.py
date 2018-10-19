8d8509dc665ff73c5e46616539efaa06607c971d
yks <yangks0522@163.com>
2018-10-19 22:25:49 +0800

    后台代码编写

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,20 +1,82 @@
 from flask import abort, current_app, g, session, json, request
 from info import db
-from info.models import News, User, Comment
+from info.models import News, User, Comment, CommentLike
 from info.modules.news import news_blue
 from flask import render_template, jsonify
 
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
 
-# 功能:回复
-# 请求路径: /news/<int:news_id>
-# 请求方式: GET
-# 请求参数:news_id
-# 返回值: detail.html页面, 用户,新闻, 热门新闻,收藏标记,新闻评论,data字典数据
 
+# 功能:点赞
+# 请求路径: /news/comment_like
+# 请求方式: POST
+# 请求参数:news_id,comment_id,action,g.user
+# 返回值: errno,errmsg
+@news_blue.route('/comment_like')
+@user_login_data
+def comment_like():
+    """
+    1.判断用户是否登陆
+    2.获取参数
+    3.校验参数
+    4.校验操作类型
+    5.通过评论编号,取出评论对象
+    6.判断评论对象是否存在
+    7.根据操作类型,点赞,或者取消点赞
+    8.返回响应
+    :return:
+    """
+    # 1.判断用户是否登陆
+    if not g.user:
+        return jsonify(errno=RET.DBERR, errmsg="用户未登陆")
+    # 2.获取参数
+    comment_id = request.json.get("comment_id")
+    action = request.json.get("action")
+    # 3.校验参数
+    if not all([comment_id, action]):
+        return jsonify(RET.NODATA, errmsg="参数不全")
+    # 4.校验操作类型
+    if not action in ["add", "remove"]:
+        return jsonify(RET.PARAMERR, msg="操作类型错误")
+    # 5.通过评论编号,取出评论对象
+    try:
+        comment = Comment.query.get(comment_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="评论获取失败")
+    # 6.判断评论对象是否存在
+    if not comment:
+        return jsonify(errno=RET.DBERR, errmsg="评论对象不存在")
+    # 7.根据操作类型,点赞,或者取消点赞
+    try:
+        if action == "add":
+            # 判断用户是否点过赞
+            comments_like = CommentLike.query.filter(CommentLike.comment_id == comment_id,CommentLike.user_id == g.user.id).first()
+            if not comment_like:
+                # 创建点赞对象,设置属性
+                comments_like = CommentLike()
+                comments_like.comment_id = comment_id
+                comments_like.user_id = g.user.id
+                # 添加到数据库
+                db.session.add(comments_like)
+                db.session.commit()
+                # 更新评论的点赞的数量
+                comment.like_count += 1
+        else:
+            # 判断用户是否点过赞
+            comments_like = CommentLike.query.filter(CommentLike.comment_id == comment_id,CommentLike.user_id == g.user.id).first()
+            if comments_like:
+                # 移除点赞对象
+                db.session.remove(comments_like)
 
-
+                # 更新点赞数量
+                comment.like_count -=1
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="操作失败")
+    # 8.返回响应
+    return jsonify(errno=RET.OK,errmsg="操作成功")
 
 
 # 功能:新闻评论
@@ -72,7 +134,7 @@ def mews_comment():
         return jsonify(errno=RET.DBERR, errmsg="评论失败")
 
     # 8.返回响应
-    return jsonify(errno=RET.OK, errmsg="评论成功",data=comment.to_dict())
+    return jsonify(errno=RET.OK, errmsg="评论成功", data=comment.to_dict())
 
 
 # 功能:收藏&取消收藏
@@ -175,10 +237,26 @@ def news_detail(news_id):
         comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR ,errmsg="获取评论失败")
+        return jsonify(errno=RET.DBERR, errmsg="获取评论失败")
+    # 获取用户的所有点赞对象
+    comment_likes = g.user.comment_likes
+
+    # 获取用户所有点赞过的评论编号
+    comment_ids = []
+    for comment_like in comment_likes:
+        comment_ids.append(comment_like.comment_id)
+
+    # 将评论对象列表转成字典列表
     comments_list = []
     for comment in comments:
-        comments_list.append(comment.to_dict())
+        com_dict = comment.to_dict()
+        com_dict["is_like"] = False
+
+        # 判断当前用户,是否对该评论点过赞
+        if g.user and comment.id in comment_ids:
+            com_dict["is_like"] = True
+
+        comments_list.append(com_dict)
 
     # 携带数据渲染页面
     data = {
@@ -186,6 +264,6 @@ def news_detail(news_id):
         "click_news_list": click_news_list,
         "user_info": g.user.to_dict() if g.user else "",
         "is_collected": is_collected,
-        "comments":comments_list
+        "comments": comments_list
     }
     return render_template("news/detail.html", data=data)
