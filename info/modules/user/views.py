6e818d9c42b9c2a7a8d5cfdd5cff66ee01ca3beb
yks <yangks0522@163.com>
2018-10-20 20:13:57 +0800

    修改密码

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -11,6 +11,39 @@ from info.utils.response_code import RET
 from . import user_blue
 
 
+# 功能:修改密码
+# 请求路径: /user/pass_info
+# 请求方式:GET,POST
+# 请求参数:GET无, POST有参数,old_password, new_password
+# 返回值:GET请求: user_pass_info.html页面,data字典数据, POST请求: errno, errmsg
+@user_blue.route('/pass_info', methods=["GET", "POST"])
+@user_login_data
+def pass_info():
+    """
+    1.获取参数
+    2.参数类型转换
+    3.分页查询
+    4.获取到分页对象属性,总页数,当前页,当前页对象
+    5.将当前也对象列表,转成字典列表
+    6.渲染页面,携带数据
+    :return:
+    """
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html")
+    # 1.获取参数
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+    # 2.校验参数
+    if not all([old_password,new_password]):
+        return jsonify(errno=RET.DBERR,errmsg="参数不全")
+    # 3.判断旧密码是否正确
+    if not g.user.check_passowrd(old_password):
+        return jsonify(errno=RET.DATAERR,errmsg="旧密码不正确")
+    # 4.设置新密码
+    g.user.password = new_password
+    # 5.返回响应
+    return jsonify(errno=RET.OK,errmsg="修改成功")
+
 # 功能:图片上传
 # 请求路径: /user/pic_info
 # 请求方式:GET,POST
@@ -29,30 +62,32 @@ def pic_info():
     :return:
     """
     if request.method == "GET":
-        return render_template("news/user_pic_info.html",user=g.user.to_dict())
+        return render_template("news/user_pic_info.html", user=g.user.to_dict())
     # 1.获取参数
     avatar = request.files.get("avatar")
     # 2.校验参数
     if not avatar:
-        return jsonify(errno=RET.DBERR,errmsg="参数不全")
+        return jsonify(errno=RET.DBERR, errmsg="参数不全")
     # 3.调用工具类方法,上传图片
     try:
         image_name = image_storage(avatar.read())
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.THIRDERR ,errmsg="七牛云上传失败")
+        return jsonify(errno=RET.THIRDERR, errmsg="七牛云上传失败")
 
     # 4.判断图片是否上传成功
     if not image_name:
-        return jsonify(errno=RET.NODATA,errmsg="上传图片失败")
+        return jsonify(errno=RET.NODATA, errmsg="上传图片失败")
     # 5.修改用户图片
     g.user.avatar_url = image_name
 
     # 6.返回响应,携带图片地址
     data = {
-        "avatar_url":constants.QINIU_DOMIN_PREFIX + image_name
+        "avatar_url": constants.QINIU_DOMIN_PREFIX + image_name
     }
-    return jsonify(errno=RET.OK,errmsg="上传成功",data=data)
+    return jsonify(errno=RET.OK, errmsg="上传成功", data=data)
+
+
 # 功能:设置获取用户基本信息
 # 请求路径: /user/base_info
 # 请求方式:GET,POST
