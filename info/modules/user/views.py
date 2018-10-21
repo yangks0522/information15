862d2fabb22a4fd766a42f2f53a256ca0bbc4753
yks <yangks0522@163.com>
2018-10-21 20:54:56 +0800

    作者other页面继承

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -7,6 +7,19 @@ from info.utils.response_code import RET
 from . import user_blue
 
 
+# 功能:作者详情页面
+# 请求路径: /user/other
+# 请求方式: GET
+# 请求参数:id
+# 返回值: 渲染other.html页面,字典data数据
+@user_blue.route('/other')
+def other_info():
+    data = {
+
+    }
+    return render_template("news/other.html", data=data)
+
+
 # 功能: 获取我的关注列表
 # 请求路径: /user/user_follow
 # 请求方式: GET
